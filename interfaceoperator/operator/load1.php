9b8bedcc443a766308807b54ec8a3b4854399815
Gihon Marten Tumbelaka <49367135+gihonmt@users.noreply.github.com>
2022-08-20 20:46:18 +0700

    Delete interfaceoperator directory

--- interfaceoperator/operator/load1.php
+++
@@ -1,8 +0,0 @@
-<!-- Source code untuk menampilkan waktu secara realtime -->
-
-<?php
-    date_default_timezone_set('Asia/Jakarta');
-    echo date('l, j F Y');
-    echo "<br>";
-    echo date('h:i:s A');
-?>
--- interfaceoperator/operator/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['usernameop']); 
-unset($_SESSION['imageop']); 
-
-session_destroy();
-header('location:../index.php');
-?>
