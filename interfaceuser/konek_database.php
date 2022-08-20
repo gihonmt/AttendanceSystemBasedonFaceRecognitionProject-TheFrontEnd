48fece31de48fef54c1be7f956cf69fe9d78645e
Gihon Marten Tumbelaka <49367135+gihonmt@users.noreply.github.com>
2022-08-20 20:56:37 +0700

    Delete interfaceuser directory

--- interfaceuser/konek_database.php
+++
@@ -1,7 +0,0 @@
-<?php
-$server = "localhost";
-$user = "root";
-$password = "";
-$database = "facepresence";
-
-$konek_database = mysqli_connect($server, $user, $password, $database) or die(mysqli_error($konek_database));
